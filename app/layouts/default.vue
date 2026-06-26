350149dba71a9abc0f554a9df59fc8e4fe665aa0
MoonAraya <maraya@usm.cl>
2026-06-26 17:46:28 -0400

    Pagina login, y default con navbar, y index vacia para que cargue el sitio,

---
+++ app/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+    <!-- NAV BAR -->
+    <div class="w-full z-50 bg-[#F5EFEF] px-6 py-4 shadow-xl">
+        <!-- para que solo use el contenido dentro de esto -->
+        <div class="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-between">
+            <!-- parte con logo o lo que sea asi -->
+            <div>
+                <h3 class="text-[#4F3D35] font-bold text-lg">Smart Events</h3>
+            </div>
+            <div class="flex flex-col gap-5 sm:flex-row items-center">
+                <!-- botones para ir a lugares -->
+                <nav class="flex items-center gap-5">
+                    <a href="#"
+                        class="text-[#4F3D35] text-md font-semibold hover:bg-[#F6D579] py-1 px-2 rounded-xl">Eventos</a>
+                    <a href="#" class="text-[#4F3D35]/70 text-md hover:bg-[#F6D579] py-1 px-2 rounded-xl">Staff</a>
+                    <a href="#"
+                        class="text-[#4F3D35]/70 text-md hover:bg-[#F6D579] py-1 px-2 rounded-xl">Inscripciones</a>
+                </nav>
+                <!-- Donde sale el nombre de usuario y boton cerrar sesión -->
+                <div class="gap-5 flex items-center">
+                    <!-- Nombre y si es admin o usuario -->
+                    <div class="flex flex-col rounded-md border-2 border-[#F6D579] px-4 py-2">
+                        <span class="text-[#4F3D35] text-md">Benjamin Marchant</span>
+                        <span class="text-[#4F3D35]/70 text-sm">Administrador</span>
+                    </div>
+
+                    <button
+                        class="bg-[#F6D579] text-[#4F3D35] py-2 px-4 rounded-xl hover:bg-[#ECB914] text-md font-bold transition-colors">Cerrar
+                        Sesion</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <main>
+        <slot />
+    </main>
+</template>
---
+++ app/pages/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex min-h-screen items-center justify-center bg-[#F5EFEF]/95">
+        <!-- div donde salen todas las cosas del login -->
+        <div
+            class="w-full max-w-md rounded-lg bg-[#CBB8A0] flex flex-col items-center p-4 sm:p-6 lg:p-8 border-2 border-[#F6D579] shadow-2xl">
+            <h2 class="font-bold text-3xl py-4 text-[#4F3D35]">Iniciar Sesion</h2>
+            <p class="mb-4 text-[#4F3D35]/70">Ingrese su correo y contraseña</p>
+            <form class="space-y-5">
+                <!-- Ingreso de correo -->
+                <div>
+                    <label class="text-[#4F3D35]">Correo Electronico</label>
+                    <input placeholder="correo@gmail.com"
+                        class="w-full border border-[#F6D579] rounded-md py-2 px-4 bg-white" />
+                </div>
+                <!-- Ingreso de contraseña -->
+                <div>
+                    <label class="text-[#4F3D35]">Contraseña</label>
+                    <input placeholder="********"
+                        class="w-full border border-[#F6D579] rounded-md py-2 px-4 bg-white" />
+                </div>
+                <!-- Boton de iniciar sesion -->
+                <button type="submit"
+                    class="w-full bg-[#F6D579] text-[#4F3D35] py-2 px-4 rounded-md hover:bg-[#ECB914] font-bold transition-colors">Iniciar
+                    Sesion</button>
+            </form>
+        </div>
+    </div>
+
+</template>
