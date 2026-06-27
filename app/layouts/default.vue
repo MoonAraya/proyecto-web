0b8ea92b485771190bfac76086ce015c190a3039
MoonAraya <maraya@usm.cl>
2026-06-26 22:21:14 -0400

    añadi colores al main y los reemplace en default y login

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -17,18 +17,18 @@ const navegacion = [
 
 <template>
     <!-- NAV BAR -->
-    <div class="w-full z-50 bg-[#F5EFEF] px-6 py-4 shadow-xl">
+    <div class="w-full z-50 bg-fondo-general px-6 py-4 shadow-xl">
         <!-- para que solo use el contenido dentro de esto -->
         <div class="max-w-7xl mx-auto flex flex-col sm:flex-row items-center gap-2 sm:justify-between">
             <!-- parte con texto y para poner logo en caso de que haga falta -->
             <div>
-                <h3 class="text-[#4F3D35] font-bold text-lg">Smart Events</h3>
+                <h3 class="text-texto font-bold text-lg">Smart Events</h3>
             </div>
             <div class="flex flex-col gap-5 sm:flex-row items-center">
                 <!-- botones para ir a lugares -->
                 <nav class="flex items-center gap-5">
                     <NuxtLink v-for="link in navegacion" :key="link.to" :to="link.to"
-                        :class="isActive(link.to) ? 'bg-[#F6D579] text-[#4F3D35] font-bold' : 'text-[#4F3D35]/70 hover:bg-[#F6D579] font-semibold'"
+                        :class="isActive(link.to) ? 'bg-boton text-texto font-bold' : 'text-texto/70 hover:bg-boton font-semibold'"
                         class="text-sm py-1 px-2 rounded-xl">
                         {{ link.label }}
                     </NuxtLink>
@@ -36,13 +36,13 @@ const navegacion = [
                 <!-- Donde sale el nombre de usuario y boton cerrar sesión -->
                 <div class="gap-5 flex items-center">
                     <!-- Nombre y si es admin o usuario -->
-                    <div class="flex flex-col rounded-md border-2 border-[#F6D579] px-4 py-2">
-                        <span class="text-[#4F3D35] text-md">Benjamin Marchant</span>
-                        <span class="text-[#4F3D35]/70 text-sm">Administrador</span>
+                    <div class="flex flex-col rounded-md border-2 border-boton px-4 py-2">
+                        <span class="text-texto text-md">Benjamin Marchant</span>
+                        <span class="text-texto/70 text-sm">Administrador</span>
                     </div>
 
                     <button
-                        class="bg-[#F6D579] text-[#4F3D35] py-2 px-4 rounded-xl hover:bg-[#ECB914] text-md font-bold transition-colors">Cerrar
+                        class="bg-boton text-texto py-2 px-4 rounded-xl hover:bg-boton-hover text-md font-bold transition-colors">Cerrar
                         Sesion</button>
                 </div>
             </div>
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="flex min-h-screen items-center justify-center bg-[#F5EFEF]/95">
+    <div class="flex min-h-screen items-center justify-center bg-fondo-general/95">
         <!-- div donde salen todas las cosas del login -->
         <div
-            class="w-full max-w-md rounded-lg bg-[#CBB8A0] flex flex-col items-center p-4 sm:p-6 lg:p-8 border-2 border-[#F6D579] shadow-2xl">
-            <h2 class="font-bold text-3xl py-4 text-[#4F3D35]">Iniciar Sesion</h2>
-            <p class="mb-4 text-[#4F3D35]/70">Ingrese su correo y contraseña</p>
+            class="w-full max-w-md rounded-lg bg-fondo-login flex flex-col items-center p-4 sm:p-6 lg:p-8 border-2 border-boton shadow-2xl">
+            <h2 class="font-bold text-3xl py-4 text-texto">Iniciar Sesion</h2>
+            <p class="mb-4 text-texto/70">Ingrese su correo y contraseña</p>
             <form class="space-y-5">
                 <!-- Ingreso de correo -->
                 <div>
-                    <label class="text-[#4F3D35]">Correo Electronico</label>
+                    <label class="text-texto">Correo Electronico</label>
                     <input placeholder="correo@gmail.com"
-                        class="w-full border border-[#F6D579] rounded-md py-2 px-4 bg-white" />
+                        class="w-full border border-boton rounded-md py-2 px-4 bg-white text-texto/70" />
                 </div>
                 <!-- Ingreso de contraseña -->
                 <div>
-                    <label class="text-[#4F3D35]">Contraseña</label>
+                    <label class="text-texto">Contraseña</label>
                     <input placeholder="********"
-                        class="w-full border border-[#F6D579] rounded-md py-2 px-4 bg-white" />
+                        class="w-full border border-boton rounded-md py-2 px-4 bg-white text-texto/70" />
                 </div>
                 <!-- Boton de iniciar sesion -->
                 <button type="submit"
-                    class="w-full bg-[#F6D579] text-[#4F3D35] py-2 px-4 rounded-md hover:bg-[#ECB914] font-bold transition-colors">Iniciar
+                    class="w-full bg-boton text-texto py-2 px-4 rounded-md hover:bg-boton-hover font-bold transition-colors">Iniciar
                     Sesion</button>
             </form>
         </div>
