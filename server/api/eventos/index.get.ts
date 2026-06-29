d8b4686af9adc3550cb004f9855c08e617498149
MoonAraya <maraya@usm.cl>
2026-06-29 19:53:16 -0400

    cards admin

--- server/api/eventos/index.get.ts
+++ server/api/eventos/index.get.ts
@@ -1,5 +1,10 @@
 export default defineEventHandler(async () => {
   return await prisma.evento.findMany({
-    orderBy: [{ fecha: 'asc' }, { titulo: 'asc' }]
+    orderBy: [{ fecha: 'asc' }, { titulo: 'asc' }],
+    include: {
+      _count: {
+        select: { inscritos:true }
+      }
+    }
   })
 })
