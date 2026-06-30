9ffffb2ba8bdd889da52c2837c7c7ce8c0fc4185
MoonAraya <maraya@usm.cl>
2026-06-29 23:52:58 -0400

    arregle post inscripcion

--- server/api/inscritos/index.post.ts
+++ server/api/inscritos/index.post.ts
@@ -3,13 +3,16 @@ export default defineEventHandler(async (event) => {
     const { email, nombre, apellido, eventoId } = await readBody(event)
 
     const emailNormalizado = typeof email === 'string' ? email.trim() : '';
-    
+    const nombreNormalizado = typeof nombre === 'string' ? nombre.trim() : '';
+    const apellidoNormalizado = typeof apellido === 'string' ? apellido.trim() : '';
+
+
     // insertar la inscripcion en la bd
     const inscripcion = await prisma.inscrito.create({
         data: {
             email: emailNormalizado,
-            nombre: nombre,
-            apellido: apellido,
+            nombre: nombreNormalizado,
+            apellido: apellidoNormalizado,
             eventoId: Number(eventoId)
         }
     })
