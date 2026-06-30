6d41f3bfd186aa16a4e38b9ca8877b2283923e4d
fvasquezg <francisco.vasquez.gatica@gmail.com>
2026-06-29 20:01:19 -0400

    gestor de cuentas terminado

--- app/pages/gestor-cuentas.vue
+++ app/pages/gestor-cuentas.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { Usuario } from '../types/usuario'
 import { z } from 'zod';
+import type { TableColumn } from '@nuxt/ui'
 
 // Para solo darle acceso al administrador a esta pagina
 definePageMeta({
@@ -108,89 +109,117 @@ function cerrarConfirmacionBorrar() {
     usuarioBorrar.value = null;
 }
 
+const columns: TableColumn<Usuario>[] = [
+    { accessorKey: 'nombre', header: 'Nombre', meta: { class: { th: 'text-center', td: 'text-center' } } },
+    { accessorKey: 'apellido', header: 'Apellido', meta: { class: { th: 'text-center', td: 'text-center' } } },
+    { accessorKey: 'email', header: 'Email', meta: { class: { th: 'text-center', td: 'text-center' } } },
+    { accessorKey: 'rol', header: 'Rol', meta: { class: { th: 'text-center', td: 'text-center' } } },
+
+    // columnas extras
+    { id: 'eliminar', header: 'Eliminar', meta: { class: { th: 'text-center', td: 'text-center' } } },
+]
+
+const tableMeta = createTableMeta<Usuario>()
 </script>
 
-
 <template>
-    <!-- Texto y boton de agregar -->
-    <div class="flex items-center justify-between">
-        <!-- Texto -->
-        <div class="space-y-2">
-            <h2 class="text-3xl font-bold text-texto">Cuentas en el sistema</h2>
-            <p class="text-texto/70">Administrar los administradores del sistema</p>
-        </div>
-        <!-- Boton de agregar -->
-        <UButton @click="mostrarFormularioAgregar = true"
-            class="bg-boton hover:bg-boton-hover text-texto py-2 px-4 rounded-md font-bold transition-colors">
-            Agregar
-            administrador</UButton>
-    </div>
-    <!-- Tarjetas con las cuentas -->
-    <section class="grid gap-6 md:grid-cols-2 lg:grid-cols-3 mt-4">
-        <CardAdmin v-for="usuario in usuarios" :key="usuario.email" :usuario="usuario"
-            @borrar-usuario="confirmarBorrarUsuario" />
-    </section>
-
-
-
-    <!-- Formulario para agregar usuario -->
-    <Popups v-model:open="mostrarFormularioAgregar" title="Agregar administrador">
-        <UForm class="space-y-4" :state="formularioNuevoUsuario" :schema="validarCrearAdmin"
-            @submit.prevent="guardarUsuario">
-            <!-- campo nombre -->
-            <UFormField label="Nombre" name="nombre" :ui="{ label: colorTextoFormulario }">
-                <UInput v-model="formularioNuevoUsuario.nombre" class="w-full" placeholder="Ej: Juanito"
-                    :ui="{ base: colorFondoCamposFormulario }" />
-            </UFormField>
-
-            <!-- campo apellido -->
-            <UFormField label="Apellido" name="apellido" :ui="{ label: colorTextoFormulario }">
-                <UInput v-model="formularioNuevoUsuario.apellido" class="w-full" placeholder="Ej: Perez"
-                    :ui="{ base: colorFondoCamposFormulario }" />
-            </UFormField>
-
-            <!-- campo email -->
-            <UFormField label="Email" name="email" :ui="{ label: colorTextoFormulario }">
-                <UInput v-model="formularioNuevoUsuario.email" class="w-full" placeholder="Ej: jperez@usm.cl"
-                    :ui="{ base: colorFondoCamposFormulario }" />
-            </UFormField>
-
-            <!-- campo contraseña -->
-            <UFormField label="Contraseña" name="password" :ui="{ label: colorTextoFormulario }">
-                <UInput v-model="formularioNuevoUsuario.password" class="w-full" type="password"
-                    placeholder="Ej: ********" :ui="{ base: colorFondoCamposFormulario }" />
-            </UFormField>
-
-            <div class="flex items-center justify-between gap-4 p-2">
-                <!-- Boton cancelar -->
-                <UButton type="button" @click="cerrarFormularioAgregar"
-                    class="bg-boton text-texto py-2 px-4 rounded-md hover:bg-boton-hover font-bold transition-colors">
-                    Cancelar
-                </UButton>
-
-                <!-- Boton agregar -->
-                <UButton type="submit" :loading="guardandoNuevoUsuario"
-                    class="bg-boton text-texto py-2 px-4 rounded-md hover:bg-boton-hover font-bold transition-colors">
-                    Agregar administrador
-                </UButton>
+    <div class="space-y-8">
+        <!-- titulo + boton para agregar usuarios nuevos -->
+        <section class="flex flex-col gap-4 lg:flex-row lg:items-end lg:justify-between">
+            <!-- TEXTOS A LA IZQUIERDA -->
+            <div>
+                <h1 class="text-3xl font-extrabold tracking-tight text-texto sm:text-4xl">
+                    STAFF
+                </h1>
+                <p class="mt-3 max-w-2xl text-base leading-7 text-texto/70">
+                    Vista para gestionar cuentas del staff.
+                </p>
             </div>
-        </UForm>
-    </Popups>
 
-    <!-- Confirmar borrar cuenta -->
-    <Popups v-model:open="mostrarConfirmacionBorrar" title="Borrar administrador"
-        :description="usuarioBorrar ? `¿Estas seguro que deseas borrar a ${usuarioBorrar.nombre} ${usuarioBorrar.apellido}? Esta decisión es permanente` : ''">
-        <!-- div con los 2 botones para cancelar o confirmar -->
-        <div class="flex justify-between items-center gap-6">
-            <!-- cancelar -->
-            <UButton @click="cerrarConfirmacionBorrar"
-                class="w-full bg-boton text-texto text-center justify-center py-2 px-4 rounded-md hover:bg-boton-hover font-bold transition-colors"
-                type="button">Cancelar</UButton>
-            <!-- confirmar -->
-            <UButton @click="borrarUsuario"
-                class="w-full bg-boton-eliminar text-white text-center justify-center py-2 px-4 rounded-md hover:bg-boton-eliminar-hover font-bold transition-colors"
-                type="button" :loading="borrandoUsuario">Confirmar</UButton>
-        </div>
-    </Popups>
+            <!-- BOTON -->
+            <UButton @click="mostrarFormularioAgregar = true"
+                class="bg-boton hover:bg-boton-hover text-texto py-2 px-4 rounded-md font-bold transition-colors">
+                Agregar
+                administrador</UButton>
+        </section>
 
+        <!-- tabla de usuarios/mantenedor con sus botones en una columna -->
+        <section>
+            <UTable :data="usuarios" :columns="columns" :meta="tableMeta"
+                class="rounded-2xl border border-brand-border bg-boton/45" :ui="{
+                    th: 'text-texto font-bold',
+                    td: 'text-texto'
+                }">
+
+                <!-- columna extra -->
+                <template #eliminar-cell="{ row }">
+                    <UButton icon="i-lucide-trash-2" size="md" color="neutral" variant="solid"
+                        class="bg-boton-eliminar hover:bg-boton-eliminar-hover focus:outline-none focus:ring-0 text-white"
+                        @click="confirmarBorrarUsuario(row.original)" />
+                </template>
+            </UTable>
+        </section>
+
+        <!-- Formulario para agregar usuario -->
+        <Popups v-model:open="mostrarFormularioAgregar" title="Agregar administrador">
+            <UForm class="space-y-4" :state="formularioNuevoUsuario" :schema="validarCrearAdmin"
+                @submit.prevent="guardarUsuario">
+                <!-- campo nombre -->
+                <UFormField label="Nombre" name="nombre" :ui="{ label: colorTextoFormulario }">
+                    <UInput v-model="formularioNuevoUsuario.nombre" class="w-full" placeholder="Ej: Juanito"
+                        :ui="{ base: colorFondoCamposFormulario }" />
+                </UFormField>
+
+                <!-- campo apellido -->
+                <UFormField label="Apellido" name="apellido" :ui="{ label: colorTextoFormulario }">
+                    <UInput v-model="formularioNuevoUsuario.apellido" class="w-full" placeholder="Ej: Perez"
+                        :ui="{ base: colorFondoCamposFormulario }" />
+                </UFormField>
+
+                <!-- campo email -->
+                <UFormField label="Email" name="email" :ui="{ label: colorTextoFormulario }">
+                    <UInput v-model="formularioNuevoUsuario.email" class="w-full" placeholder="Ej: jperez@usm.cl"
+                        :ui="{ base: colorFondoCamposFormulario }" />
+                </UFormField>
+
+                <!-- campo contraseña -->
+                <UFormField label="Contraseña" name="password" :ui="{ label: colorTextoFormulario }">
+                    <UInput v-model="formularioNuevoUsuario.password" class="w-full" type="password"
+                        placeholder="Ej: Al menos 8 caracteres." :ui="{ base: colorFondoCamposFormulario }" />
+                </UFormField>
+
+                <div class="flex items-center justify-between gap-4 p-2">
+                    <!-- Boton cancelar -->
+                    <UButton type="button" @click="cerrarFormularioAgregar"
+                        class="bg-boton text-texto py-2 px-4 rounded-md hover:bg-boton-hover font-bold transition-colors">
+                        Cancelar
+                    </UButton>
+
+                    <!-- Boton agregar -->
+                    <UButton type="submit" :loading="guardandoNuevoUsuario"
+                        class="bg-boton text-texto py-2 px-4 rounded-md hover:bg-boton-hover font-bold transition-colors">
+                        Agregar administrador
+                    </UButton>
+                </div>
+            </UForm>
+        </Popups>
+
+
+        <!-- modal para eliminar usuario -->
+        <!-- Confirmar borrar cuenta -->
+        <Popups v-model:open="mostrarConfirmacionBorrar" title="Borrar administrador"
+            :description="usuarioBorrar ? `¿Estas seguro que deseas borrar a ${usuarioBorrar.nombre} ${usuarioBorrar.apellido}? Esta decisión es permanente` : ''">
+            <!-- div con los 2 botones para cancelar o confirmar -->
+            <div class="flex justify-between items-center gap-6">
+                <!-- cancelar -->
+                <UButton @click="cerrarConfirmacionBorrar"
+                    class="w-full bg-boton text-texto text-center justify-center py-2 px-4 rounded-md hover:bg-boton-hover font-bold transition-colors"
+                    type="button">Cancelar</UButton>
+                <!-- confirmar -->
+                <UButton @click="borrarUsuario"
+                    class="w-full bg-boton-eliminar text-white text-center justify-center py-2 px-4 rounded-md hover:bg-boton-eliminar-hover font-bold transition-colors"
+                    type="button" :loading="borrandoUsuario">Confirmar</UButton>
+            </div>
+        </Popups>
+    </div>
 </template>
